39a9d1e02f3a4c5d5a4d6ccc3026e67b9181616d
vagelim <vagelim@users.noreply.github.com>
2021-03-06 14:46:18 -0500

    Create setChromeDefault.ps1

---
+++ setChromeDefault.ps1
@@ -0,0 +1,11 @@
+function Set-DefaultBrowser {
+Add-Type -AssemblyName 'System.Windows.Forms'
+Start-Process $env:windir\system32\control.exe -LoadUserProfile -Wait -ArgumentList '/name Microsoft.DefaultPrograms /page pageDefaultProgram\pageAdvancedSettings?pszAppName=Chrome'
+Sleep 2
+[System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{TAB}{TAB}{TAB} ")
+Sleep 2
+[System.Windows.Forms.SendKeys]::SendWait("{DOWN}{DOWN} {DOWN}{DOWN}{DOWN}{DOWN}{DOWN} {DOWN} {TAB} ")
+Sleep 2
+[System.Windows.Forms.SendKeys]::SendWait("%{F4}")
+}
+Set-DefaultBrowser
